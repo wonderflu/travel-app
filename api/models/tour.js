f39b9865e8aa0a2acc71408e47abceea9cf6009c
wonderflu <flupwnzx@gmail.com>
2022-10-08 20:20:30 +0200

    added sorting for tours

--- api/models/tour.js
+++ api/models/tour.js
@@ -1,57 +1,58 @@
 const { Schema, model } = require('mongoose');
 
-const tourSchema = new Schema({
-  name: {
-    type: String,
-    required: [true, 'Name field cannot be empty'],
-    unique: true,
-    trim: true,
+const tourSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Name field cannot be empty'],
+      unique: true,
+      trim: true,
+    },
+    duration: {
+      type: Number,
+      required: [true, 'A tour must have a duration'],
+    },
+    maxGroupSize: {
+      type: Number,
+      required: [true, 'A tour must have a group size'],
+    },
+    difficulty: {
+      type: String,
+      required: [true, 'A tour must have a difficulty'],
+    },
+    ratingsQuantity: {
+      type: Number,
+      default: 0,
+    },
+    ratingsAverage: {
+      type: Number,
+      default: 4.5,
+    },
+    price: {
+      type: Number,
+      required: [true, 'A tour must have a price'],
+    },
+    priceDiscount: Number,
+    summary: {
+      type: String,
+      trim: true,
+      required: [true, 'A tour must have a description'],
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    imageCover: {
+      type: String,
+      required: [true, 'A tour must have a cover image'],
+    },
+    images: [String],
+    startDates: [Date],
   },
-  duration: {
-    type: Number,
-    required: [true, 'A tour must have a duration'],
-  },
-  maxGroupSize: {
-    type: Number,
-    required: [true, 'A tour must have a group size'],
-  },
-  difficulty: {
-    type: String,
-    required: [true, 'A tour must have a difficulty'],
-  },
-  ratingsQuantity: {
-    type: Number,
-    default: 0,
-  },
-  ratingsAverage: {
-    type: Number,
-    default: 4.5,
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price'],
-  },
-  priceDiscount: Number,
-  summary: {
-    type: String,
-    trim: true,
-    required: [true, 'A tour must have a description'],
-  },
-  description: {
-    type: String,
-    trim: true,
-  },
-  imageCover: {
-    type: String,
-    required: [true, 'A tour must have a cover image'],
-  },
-  images: [String],
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-  },
-  startDates: [Date],
-});
+  {
+    timestamps: true,
+  }
+);
 
 const Tour = model('Tour', tourSchema);
 
