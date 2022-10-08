655e3f2ef25dde529c4d52d63fc6395620b75c91
wonderflu <flupwnzx@gmail.com>
2022-10-08 20:36:54 +0200

    added error handler

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ const app = express();
 const router = require('./api/routes/index');
 
 const { PORT, MONGO_DB } = require('./configs');
+const errorHandler = require('./api/middlewares/errorHandler');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use('/api', router);
+app.use(errorHandler);
 
 mongoose.connect(MONGO_DB).then(() => {
   console.log('Connected to MongoDB.');
