50ae3e79582d19da440c67b131b92b935ee8ba65
wonderflu <flupwnzx@gmail.com>
2022-10-09 20:09:00 +0200

    added token service

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -3,6 +3,7 @@ const AuthService = require('../services/auth');
 class AuthController {
   async signup(req, res) {
     const user = req.body;
+
     const newUser = await AuthService.signup(user);
 
     res.status(201).json({ newUser });
